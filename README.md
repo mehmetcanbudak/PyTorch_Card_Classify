5d43e08bbc0c4d912c1d45edaac4f0312bfc122a
Mehmmetcan Budak <mehmetcanbudak@gmail.com>
2024-02-01 00:12:25 +0300

    Initialized repo with readme and gitignore

---
+++ README.md
@@ -0,0 +1,20 @@
+
+![App Screenshot](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeSoUspoNepB9nTtyKnui2IA32nM_m3PhJ19fjwkOemb0UvwCsGfaSBmA64TqJq72C1Os&usqp=CAU)
+
+
+# Card Classification with PyTorch
+
+Build your very first PyTorch model that can classify images of playing cards
+
+## Installation
+
+Install my-project with npm
+
+```bash
+  pip install -r requirements.txt
+```
+    
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
---
+++ model.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+import timm
+
+import matplotlib.pyplot as plt # For data viz
+import pandas as pd
+import numpy as np
+import sys
+from tqdm.notebook import tqdm
+
+print('System Version:', sys.version)
+print('PyTorch version', torch.__version__)
+print('Torchvision version', torchvision.__version__)
+print('Numpy version', np.__version__)
+print('Pandas version', pd.__version__)
