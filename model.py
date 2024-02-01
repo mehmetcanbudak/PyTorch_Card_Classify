9ea17006486d2c1b8459cc29e78ab70afed3a172
Mehmmetcan Budak <mehmetcanbudak@gmail.com>
2024-02-01 03:33:05 +0300

    changes

--- model.py
+++ model.py
@@ -1,19 +1,18 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
-import torchvision
-import torchvision.transforms as transforms
-from torchvision.datasets import ImageFolder
-import timm
-import matplotlib.pyplot as plt
-from PIL import Image
+import sys
 from glob import glob
 
 import matplotlib.pyplot as plt  # For data viz
-import pandas as pd
 import numpy as np
-import sys
+import pandas as pd
+import timm
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from PIL import Image
+from torch.utils.data import DataLoader, Dataset
+from torchvision.datasets import ImageFolder
 from tqdm import tqdm
 
 print("System Version:", sys.version)
